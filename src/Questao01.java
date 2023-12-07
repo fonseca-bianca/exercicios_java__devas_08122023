4e5d64253730568d8bb3446144de09c43db115f6
Bianca Fonseca <103074990+fonseca-bianca@users.noreply.github.com>
2023-12-07 13:56:30 -0300

    Update Questao01.java
    Enviando resposta questão 01

--- src/Questao01.java
+++ src/Questao01.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Exercicio01 {
+import java.util.Scanner;
+
+public class Questao01 {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in); //pra receber os dados
+
+        System.out.print("Insira o seu nome: ");
+        String nome = input.nextLine(); //avança scanner pra próxima linha e retorna entrada
+
+        System.out.print("Insira a sua profissão: ");
+        String profissao = input.nextLine();
+
+
+        System.out.print("Insira a sua idade: ");
+        int idade = input.nextInt();
+        input.nextLine();
+
+        System.out.printf("%s\n", nome); //"\n" pra quebrar linha
+        System.out.print(profissao);
+        System.out.println("\n" + idade);
+    }
 }
